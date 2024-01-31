49fff268b5fb3dd9b559793e78fe1c5dfe5dab08
oyewole timilehin <oyewoleoluwatimilehin2@gmail.com>
2024-01-31 09:34:49 -1100

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar.jsx";
+import axios from "axios";
 
 function Header() {
+  const [responses, setResponse] = useState([]);
+
+  const topRated = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+        {
+          headers: {
+            accept: "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjRmZDJlM2MzYTc5N2JmMDhmZDM3NjhhYzYyYmE5OSIsInN1YiI6IjY1YmEzMGU3ZTlkYTY5MDE3YmY1ZWYzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h0D2I4SCXYCfendFQfyTwrf7YZO3tbE9Pf0XLEdzdKE",
+          },
+        }
+      );
+      console.log("Response", response);
+      console.log("Data", response.data);
+      console.log("Result", response.data.results);
+      setResponse(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    topRated();
+  }, []);
+
+  console.log(responses);
   return (
-    <div className=" h-screen bg-black">
+    <div className=" h-screen w-full bg-black">
       <Navbar />
+      <div className=" "></div>
     </div>
   );
 }
